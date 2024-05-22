55a2c09e649e1773fdabd1d79aaef55d49a0a2ba
ab-h-i-n <abhinshelly@gmail.com>
2024-05-22 19:24:46 +0530

    social icons changed

--- src/components/home/CountCard.jsx
+++ src/components/home/CountCard.jsx
@@ -3,14 +3,14 @@ import Odometer from './Odometer'
 
 const CountCard = ({value , text}) => {
   return (
-    <div className="grid place-items-center ">
+    <div className="grid place-items-center lg:flex lg:items-center lg:gap-2">
       {/* count  */}
-      <div className="flex items-center font-bold text-xl lg:text-3xl">
+      <div className="flex items-center font-bold text-3xl lg:text-6xl w-min">
         <Odometer value={value} />
         <div>+</div>
       </div>
       {/* text  */}
-      <div className="font-medium text-center lg:text-lg">{text}</div>
+      <div className="font-medium text-center lg:text-start lg:text-lg w-min opacity-50">{text}</div>
     </div>
   );
 };
