4ba0d83e70d0c9465dc3d1e5217fe59f2c7887ed
ab-h-i-n <abhinshelly@gmail.com>
2024-05-25 16:32:17 +0530

    added metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,18 +2,21 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "NoTime",
   description: "Master Modern Web Development With a Project Based Approach",
-  "og:image" : "https://i.ibb.co/M88xDfy/Home-Page-SEO.png"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:image" content="/website-thumbnail.png" />
+      </Head>
       <body className={inter.className}>
         <Header />
         {children}
