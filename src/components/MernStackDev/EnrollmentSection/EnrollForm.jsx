018cea77bcf1744d541bd115adeb156f0f9a016e
ab-h-i-n <abhinshelly@gmail.com>
2024-08-31 19:52:31 +0530

    hidded upload screenshot from enrollment form

--- src/components/MernStackDev/EnrollmentSection/EnrollForm.jsx
+++ src/components/MernStackDev/EnrollmentSection/EnrollForm.jsx
@@ -113,20 +113,20 @@ const SheduleTestForm = ({ isOpen, setFormOpen }) => {
             />
 
             {/* transaction photo  */}
-            <div className="w-full">
+            {/* <div className="w-full">
               <input type="file" name="transaction_photo" id="transaction_photo" onChange={handleImageUpload} className="hidden" />
 
-              <label htmlFor="transaction_photo" className=" flex items-center gap-3 cursor-pointer lg:text-xl focus:outline-none focus:border-white bg-transparent border-b border-b-[#1c222e] text-[#9ca3af] w-full p-2" >
+              <label htmlFor="transaction_photo" className=" flex items-center gap-3 cursor-pointer lg:text-xl focus:outline-none focus:border-white bg-transparent border-b border-b-[#1c222e] text-[#9ca3af] w-full p-2" > */}
 
                 {/* icon  */}
-                <img src="/upload.svg" alt="upload" className="w-8" />
+                {/* <img src="/upload.svg" alt="upload" className="w-8" /> */}
 
                 {/* text  */}
-                <p className="ml-2 max-w-[350px] text-ellipsis overflow-hidden text-nowrap ">{imageUploadPlaceholder}</p>
+                {/* <p className="ml-2 max-w-[350px] text-ellipsis overflow-hidden text-nowrap ">{imageUploadPlaceholder}</p> */}
 
-              </label>
+              {/* </label>
 
-            </div>
+            </div> */}
 
             {/* buttons */}
             <div className="flex items-center gap-5 relative z-[49]">
