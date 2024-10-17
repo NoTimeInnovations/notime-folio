4de322b1d14e2d7136bba3f31791320b3aeebf7d
NoTimeInnovations <projects.notime@gmail.com>
2024-10-17 22:57:24 +0530

    fix : payment redirection

--- src/app/api/payment/route.js
+++ src/app/api/payment/route.js
@@ -1,30 +1,29 @@
 import CCAvenue from "@/utils/CCAvenue";
 import { cookies } from "next/headers";
 
+export async function POST(req, res) {
 
-export async function POST(req , res) {
+  const reqUrl = new URL(req.url);
+  const courseId = reqUrl.searchParams.get("id");
+  const userId = JSON.parse(cookies().get("user")?.value)?.id;
+  const authToken = cookies().get("auth_token")?.value;
+
+  console.log({
+    courseId,
+    authToken,
+    userId,
+    data,
+  });
+
+  if (!courseId || !authToken || !userId) {
+    res.redirect(`/courses/${courseId}?error=payment-failed`);
+  }
+
   try {
-    // Decrypt the Response Data from Request Body
-    let data = CCAvenue.redirectResponseToJson(req.body.encResp);
-    const reqUrl = new URL(req.url);
-    const courseId = reqUrl.searchParams.get("id");
-    const userId = JSON.parse(cookies().get('user')?.value)?.id;
-    const authToken = cookies().get('auth_token')?.value;
-
-    console.log({
-      courseId,
-      authToken,
-      userId,
-      data,
-    });
     
-
-    if(!courseId || !authToken || !userId){
-      res.redirect(`/courses/${courseId}?error=payment-failed`);
-    }
+    let data = CCAvenue.redirectResponseToJson(req.body.encResp);
 
     if (data.order_status === "Success") {
-
       try {
         const response = await fetch(
           `${process.env.NEXT_PUBLIC_PAYLOAD_URL}/api/users/${userId}`,
@@ -41,17 +40,15 @@ export async function POST(req , res) {
         );
         const user = await response.json();
 
-        if(user?.errors?.length > 0){
+        if (user?.errors?.length > 0) {
           throw new Error("Error enrolling student to course");
         }
-
       } catch (error) {
         console.error("Error enrolling student to course:", error);
         res.redirect(`/courses/${courseId}?error=payment-failed`);
       }
 
       res.redirect(`/courses/${courseId}?success=payment-success`);
-
     } else {
       res.redirect(`/courses/${courseId}?error=payment-failed`);
     }
