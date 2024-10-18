16ae0db8345bdee1895b9f456831ebbb890c2886
NoTimeInnovations <projects.notime@gmail.com>
2024-10-18 12:53:35 +0530

    fix ; redirect error

--- src/app/api/payment/route.js
+++ src/app/api/payment/route.js
@@ -6,22 +6,27 @@ export async function POST(req) {
     // Decrypt the Response Data from Request Body
     
     const reqBody = await req.json();
+
+    console.log('reqBody',reqBody);
+    console.log('reqBody.encResp',reqBody.encResp);
+    
+    
     
     let data = CCAvenue.redirectResponseToJson(reqBody.encResp);
 
     // Handle Redirect as per Payment Status
     if (data.order_status === "Success") {
       // Redirect to Payment Success Page
-      return Response.redirect(302, "https://www.notime.co.in/dashboard");
+      return Response.redirect("/dashboard");
     } else {
       // Redirect to Payment Failed Page
-      return Response.redirect(302, "https://www.notime.co.in");
+      return Response.redirect("/");
     }
   } catch (error) {
     // Handling Errors if anything Issue/Problem while Payment
     console.error("Error processing CCAvenue request:", error);
 
     // Redirect to Payment Failed Page
-    return Response.redirect(302, "https://www.notime.co.in");
+    return Response.redirect("/");
   }
 }
