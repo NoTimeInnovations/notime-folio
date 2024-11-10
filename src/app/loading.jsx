997410b7c9ff5fa8d6a7068d55f4fca6f7622a9d
ab-h-i-n <abhinshelly03@gmail.com>
2024-11-10 13:23:24 +0530

    added loading

---
+++ src/app/loading.jsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="min-h-screen w-full grid place-items-center">
+      <div className="loader">
+        <div className="orbe" style={{ "--index": 0 }}></div>
+        <div className="orbe" style={{ "--index": 1 }}></div>
+        <div className="orbe" style={{ "--index": 2 }}></div>
+        <div className="orbe" style={{ "--index": 3 }}></div>
+        <div className="orbe" style={{ "--index": 4 }}></div>
+      </div>
+    </div>
+  );
+};
+
+export default loading;
